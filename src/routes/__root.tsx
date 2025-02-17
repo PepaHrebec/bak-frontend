0f529503aca7ad57fb957a3560fb4eebf1e5e42b
Josef Hřebec <josefhrebec02@gmail.com>
2025-02-17 15:33:26 +0100

    feat: add additional route and index foos

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,36 +1,48 @@
-import * as React from 'react'
-import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { Toaster } from "react-hot-toast";
+import { useUserStore } from "../utils/store";
 
 export const Route = createRootRoute({
   component: RootComponent,
-})
+});
 
 function RootComponent() {
+  const { user } = useUserStore();
+
   return (
     <>
       <div className="p-2 flex gap-2 text-lg">
         <Link
           to="/"
           activeProps={{
-            className: 'font-bold',
+            className: "font-bold",
           }}
           activeOptions={{ exact: true }}
         >
           Home
-        </Link>{' '}
+        </Link>{" "}
         <Link
           to="/about"
           activeProps={{
-            className: 'font-bold',
+            className: "font-bold",
           }}
         >
           About
         </Link>
+        <Link
+          to="/excercises"
+          activeProps={{
+            className: "font-bold",
+          }}
+        >
+          Excercises
+        </Link>
+        {user !== undefined ? <div>{user.name}</div> : <div>Log in</div>}
       </div>
-      <hr />
       <Outlet />
       <TanStackRouterDevtools position="bottom-right" />
+      <Toaster />
     </>
-  )
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,32 +1,60 @@
-import * as React from "react";
+import { useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
 import { load } from "../main";
 import { useQuery } from "@tanstack/react-query";
 import Keyboard from "../components/keyboard";
+import toast from "react-hot-toast";
 
 export const Route = createFileRoute("/")({
   component: HomeComponent,
 });
 
 function HomeComponent() {
-  const { isPending, error, data, refetch } = useQuery({
+  const { isPending, error, data, isFetching, refetch } = useQuery({
     queryKey: ["repoData"],
     queryFn: () => load(),
   });
 
-  if (isPending) return "Loading...";
+  const [userWord, setUserWord] = useState("");
+
+  if (isPending || isFetching) return "Loading...";
 
   if (error) return "An error has occurred: " + error.message;
 
+  const transcription = data.transcriptions[0];
+
+  function letterClicked(e: React.MouseEvent<Element, MouseEvent>) {
+    const letter = (e.target as HTMLElement).innerText;
+    setUserWord((prev) => prev + letter);
+  }
+
+  function deleteLetter() {
+    if (userWord.length === 0) {
+      return;
+    }
+    setUserWord((prev) => prev.slice(0, prev.length - 1));
+  }
+
+  function check() {
+    console.log(transcription, userWord);
+    if (transcription === userWord) {
+      toast.success("Good");
+    } else if (transcription.replace("ˈ", "") === userWord) {
+      toast("Almost there!");
+    } else {
+      console.log("Hi");
+      toast.error("Bad");
+    }
+  }
+
   return (
     <div className="p-2">
-      <h3>Welcome {data.word}!</h3>
-      <div>{data.english}</div>
+      <h3>Welcome {data.originalWord}!</h3>
+      <h2>{userWord}</h2>
+      <div>{data.transcriptions[0]}</div>
       <button onClick={() => refetch()}>Refetch</button>
-      <Keyboard
-        clickLetterBtn={(e) => console.log((e.target as HTMLElement).innerText)}
-        clickDeleteBtn={() => console.log("Del")}
-      />
+      <button onClick={() => check()}>Check</button>
+      <Keyboard clickLetterBtn={letterClicked} clickDeleteBtn={deleteLetter} />
     </div>
   );
 }
