bbba80507a96a85abe86062942091cefa603e772
Josef Hřebec <josefhrebec02@gmail.com>
2025-02-22 18:55:32 +0100

    feat: library updates

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,6 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Button } from "../components/buttons/Button";
-import { Search, Send, NotebookPen } from "lucide-react";
 
 export const Route = createFileRoute("/about")({
   component: AboutComponent,
