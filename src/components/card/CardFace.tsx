2b709dbeebaff720b1da3d8a1e5da879f5184065
Josef Hřebec <josefhrebec02@gmail.com>
2025-02-28 00:16:29 +0100

    fix: fix card hover on mobile

--- src/components/card/CardFace.tsx
+++ src/components/card/CardFace.tsx
@@ -2,24 +2,29 @@ import { cn } from "../../utils/cn";
 
 interface CardFaceProps {
   words: string[] | undefined;
+  isTranscription: boolean;
   isBack?: boolean;
 }
 
-export const CardFace = ({ isBack = false, words }: CardFaceProps) => {
+export const CardFace = ({
+  isBack = false,
+  words,
+  isTranscription,
+}: CardFaceProps) => {
   return (
     <div
       className={cn(
-        isBack ? "[transform:rotateY(180deg)]" : "",
-        "absolute inset-0 h-full w-full rounded-md [backface-visibility:hidden] bg-white p-2 flex flex-col items-center justify-center"
+        "absolute inset-0 h-full w-full rounded-md [backface-visibility:hidden] bg-white p-2 flex flex-col items-center justify-center transition duration-500",
+        isBack ? "[transform:rotateY(180deg)]" : ""
       )}
     >
       {words ? null : <p className="text-gray-400">Loading...</p>}
       {words?.map((word) => {
         return (
           <div key={word} className="flex flex-row">
-            {isBack ? null : <p className="text-gray-400">/</p>}
+            {isTranscription ? <p className="text-gray-400">/</p> : null}
             <p>{word}</p>
-            {isBack ? null : <p className="text-gray-400">/</p>}
+            {isTranscription ? <p className="text-gray-400">/</p> : null}
           </div>
         );
       })}
--- src/components/card/RotaryCard.tsx
+++ src/components/card/RotaryCard.tsx
@@ -1,22 +1,73 @@
+import { useState } from "react";
+import { cn } from "../../utils/cn";
 import { CardFace } from "./CardFace";
 
 interface RotaryCardProps extends React.HTMLAttributes<HTMLDivElement> {
   originalWord: string | undefined;
   transcribedWords: string[] | undefined;
+  transcriptionIsFront?: boolean;
 }
 
 export const RotaryCard = ({
   originalWord,
   transcribedWords,
+  transcriptionIsFront = true,
+  className,
   ...props
 }: RotaryCardProps) => {
+  // Clicking the card disables the hover attribute and straight-up applies the rotation
+  // transformation which would've been applied through the hover, making sure that
+  // the effects don't overlap in a case of a click AND a hover
+  const [rotate, setRotate] = useState(true);
+
   return (
-    <div {...props} className="group h-52 w-52 [perspective:1000px]">
-      <div className="relative h-full w-full rounded-md shadow-md transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+    <div
+      {...props}
+      className={cn("group h-52 w-52 [perspective:1000px]", className)}
+      onClick={() => setRotate((prev) => !prev)}
+    >
+      <div
+        className={cn(
+          "relative h-full w-full rounded-md shadow-md transition-all duration-500 [transform-style:preserve-3d]",
+          rotate
+            ? "group-hover:[transform:rotateY(180deg)]"
+            : "[transform:rotateY(180deg)]"
+        )}
+      >
         {/* Front Face */}
-        <CardFace words={transcribedWords} />
+        {transcriptionIsFront ? (
+          <>
+            <CardFace
+              isBack={!transcriptionIsFront}
+              words={transcribedWords}
+              isTranscription={true}
+            />
+            <CardFace
+              isBack={transcriptionIsFront}
+              words={originalWord ? [originalWord] : undefined}
+              isTranscription={false}
+            />
+          </>
+        ) : (
+          <>
+            <CardFace
+              isBack={transcriptionIsFront}
+              words={originalWord ? [originalWord] : undefined}
+              isTranscription={false}
+            />
+            <CardFace
+              isBack={!transcriptionIsFront}
+              words={transcribedWords}
+              isTranscription={true}
+            />
+          </>
+        )}
+        {/* <CardFace isBack={!isTranscriptionFront} words={transcribedWords} /> */}
         {/* Back Face */}
-        <CardFace isBack words={originalWord ? [originalWord] : undefined} />
+        {/* <CardFace
+          isBack={isTranscriptionFront}
+          words={originalWord ? [originalWord] : undefined}
+        /> */}
       </div>
     </div>
   );
