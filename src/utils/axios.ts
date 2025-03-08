e279633cedc3b910a7190b302b6c0fc16d0c506b
Josef Hřebec <josefhrebec02@gmail.com>
2025-03-08 17:57:09 +0100

    feat: add .env

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetcher = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_BACKEND,
   timeout: 1000,
   withCredentials: true,
 });
