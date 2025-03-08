e3afe25457cde5c6e3283a5040fb503bec5de9ee
Josef Hřebec <josefhrebec02@gmail.com>
2025-03-08 18:47:14 +0100

    fix: axios url

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetcher = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://bak-backend-production.up.railway.app/",
   timeout: 1000,
   withCredentials: true,
 });
